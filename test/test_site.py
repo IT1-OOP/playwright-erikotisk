4b1e17145bcaa9051e0108daa95a3ee6fad95dc1
erikotisk <erik.otisk@student.vitkovickastredni.cz>
2025-04-09 12:26:16 +0200

    nejak

--- test/test_site.py
+++ test/test_site.py
@@ -11,5 +11,11 @@ def test_page_title():
         page = browser.new_page()
         page.goto(f"file:///{cesta}")
         nadpis_1 = page.locator('h1').first
+        nadpis_text=page.locator('text="Nadpis 1"')
+        div_1 = page.locator('.container')
         expect(nadpis_1).to_be_visible()
+        expect(nadpis_text).to_be_visible()
+        expect(div_1).to_be_visible()
+        expect(nadpis_text).to_have_count(1)
+
         browser.close()
